46180fbb39dae75cdeccd3de011e62c0a199b22f
Chris Wong <lambda.fairy@gmail.com>
2016-12-20 12:46:46 +1300

    Add an extension trait to test code

--- tests/tests.rs
+++ tests/tests.rs
@@ -49,24 +49,45 @@ impl Acidic for Ponies {
     }
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Deserialize, Serialize)]
 enum PoniesQueryEvent {
     get_pony(String),
 }
 
-#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[derive(Deserialize, Serialize)]
 enum PoniesQueryOutput {
     get_pony(Option<u32>),
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Deserialize, Serialize)]
 enum PoniesUpdateEvent {
     set_pony(String, u32),
 }
 
-#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[derive(Deserialize, Serialize)]
 enum PoniesUpdateOutput {
     set_pony(()),
+}
+
+trait AcidPoniesExt {
+    fn get_pony(&self, name: String) -> lewis::Result<Option<u32>>;
+    fn set_pony(&self, name: String, value: u32) -> lewis::Result<()>;
+}
+
+impl AcidPoniesExt for Acid<Ponies> {
+    fn get_pony(&self, name: String) -> lewis::Result<Option<u32>> {
+        Ok(match self.query(PoniesQueryEvent::get_pony(name))? {
+            PoniesQueryOutput::get_pony(r) => r,
+            // _ => unreachable!()
+        })
+    }
+
+    fn set_pony(&self, name: String, value: u32) -> lewis::Result<()> {
+        Ok(match self.update(PoniesUpdateEvent::set_pony(name, value))? {
+            PoniesUpdateOutput::set_pony(r) => r,
+            // _ => unreachable!()
+        })
+    }
 }
 
 #[test]
@@ -74,14 +95,14 @@ fn smoke() {
     let root = TempDir::new("lewis").unwrap();
     {
         let acid = Acid::<Ponies>::open(root.path()).unwrap();
-        let r = acid.update(PoniesUpdateEvent::set_pony("Pinkie Pie".into(), 21)).unwrap();
-        assert_eq!(r, PoniesUpdateOutput::set_pony(()));
-        let r = acid.query(PoniesQueryEvent::get_pony("Pinkie Pie".into())).unwrap();
-        assert_eq!(r, PoniesQueryOutput::get_pony(Some(21)));
+        let r = acid.set_pony("Pinkie Pie".into(), 21).unwrap();
+        assert_eq!(r, ());
+        let r = acid.get_pony("Pinkie Pie".into()).unwrap();
+        assert_eq!(r, Some(21));
     }
     {
         let acid = Acid::<Ponies>::open(root.path()).unwrap();
-        let r = acid.query(PoniesQueryEvent::get_pony("Pinkie Pie".into())).unwrap();
-        assert_eq!(r, PoniesQueryOutput::get_pony(Some(21)));
+        let r = acid.get_pony("Pinkie Pie".into()).unwrap();
+        assert_eq!(r, Some(21));
     }
 }
